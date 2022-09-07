bbef44c4b8be6f1d1dd9ed61f537a73553b64681
Gregory Cox <gregcox7@gmail.com>
2022-09-07 08:12:26 -0400

    Upload through class 5

---
+++ DBDA2Eprograms/SimpleGraph.R
@@ -0,0 +1,3 @@
+x = seq( from = -3 , to = 3 , by = 0.1 )   # Specify vector of x values.
+y = x^3                                    # Specify corresponding y values.
+plot( x , y , col="purple" , type="l" )   # Plot the x,y points as a blue line.
